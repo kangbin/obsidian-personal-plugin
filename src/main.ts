7fcae6d00901135163ecda15a9e72eafde0232c9
kangbin <binarykang@163.com>
2024-10-20 19:15:14 +0800

    feat: add DatePlugjin

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,30 @@
 import { type App, Plugin, type PluginManifest } from "obsidian";
+import { DatePlugin } from "./plugins/date-plugin";
 import { IconPlugin } from "./plugins/icon-plugin";
 import { info } from "./utils/logger";
 
 export default class PersonalPlugin extends Plugin {
+  private datePlugin: DatePlugin;
   private iconPlugin: IconPlugin;
 
   constructor(app: App, manifest: PluginManifest) {
     super(app, manifest);
 
+    this.datePlugin = new DatePlugin(app, manifest);
     this.iconPlugin = new IconPlugin(app, manifest);
   }
 
   async onload() {
     info(`PersonalPlugin onload`);
 
+    this.datePlugin.onload();
     this.iconPlugin.onload();
   }
 
   onunload() {
     info(`PersonalPlugin onunload`);
 
+    this.datePlugin.onunload();
     this.iconPlugin.onunload();
   }
 }
---
+++ src/plugins/date-plugin/index.ts
@@ -0,0 +1,69 @@
+import {
+  type App,
+  type Editor,
+  type Menu,
+  type PluginManifest,
+  Plugin,
+  moment,
+} from "obsidian";
+import { info } from "../../utils/logger";
+
+export class DatePlugin extends Plugin {
+  constructor(app: App, manifest: PluginManifest) {
+    super(app, manifest);
+  }
+
+  async onload() {
+    info(`DatePlugin onload`);
+
+    const {
+      app: { workspace },
+    } = this;
+
+    workspace.on("editor-menu", this.handleEditorMenu);
+  }
+
+  onunload() {
+    info(`DatePlugin onunload`);
+
+    const {
+      app: { workspace },
+    } = this;
+
+    workspace.off("editor-menu", this.handleEditorMenu);
+  }
+
+  private handleEditorMenu = (menu: Menu, editor: Editor) => {
+    menu.addItem((item) => {
+      console.log(`KBF.item`, item);
+      item.setTitle("插入时间");
+
+      // https://github.com/mProjectsCode/obsidian-meta-bind-plugin/blob/4b16a75fb63dfdb34e3ccf2756a324a84dd8fd85/packages/obsidian/src/EditorMenu.ts#L17-L30
+      // https://forum.obsidian.md/t/make-setsubmenu-public-api/59175/2
+      this.generateSubmenu(editor, (item as any).setSubmenu());
+    });
+  };
+
+  private generateSubmenu(editor: Editor, submenu: Menu) {
+    const submenuTexts: Array<string> = [
+      moment().format("YYYY-MM-DD"),
+      moment().format("## MM-DD ddd"),
+    ];
+
+    submenuTexts.forEach((title) => {
+      submenu.addItem((item) => {
+        item.setTitle(title);
+        item.onClick(() => this.insertText(editor, title));
+      });
+    });
+  }
+
+  private insertText(editor: Editor, text: string) {
+    const cursor = editor.getCursor();
+    editor.replaceRange(text, cursor);
+    editor.setCursor({
+      line: cursor.line,
+      ch: cursor.ch + text.length,
+    });
+  }
+}
