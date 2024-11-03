e3a0716f47202c5f18c5d83eb83f8e822e2cc900
kangbin <binarykang@163.com>
2024-11-03 22:36:55 +0800

    chore: remove comment.

--- src/plugins/date-plugin/index.ts
+++ src/plugins/date-plugin/index.ts
@@ -35,7 +35,6 @@ export class DatePlugin extends Plugin {
 
   private handleEditorMenu = (menu: Menu, editor: Editor) => {
     menu.addItem((item) => {
-      console.log(`KBF.item`, item);
       item.setTitle("插入时间");
 
       // https://github.com/mProjectsCode/obsidian-meta-bind-plugin/blob/4b16a75fb63dfdb34e3ccf2756a324a84dd8fd85/packages/obsidian/src/EditorMenu.ts#L17-L30
